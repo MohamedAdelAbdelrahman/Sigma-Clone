ef7e33b26725b7c64c463c6e40da5784ceb8cb69
“abdalrhman772 <abdalrhmanmahmoud27@gmail.com>
2023-02-24 15:14:59 +0200

    cart & account component added, cart first try UI added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
