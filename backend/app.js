60c5d28dcaad4b0cf39b6fb53c1a0a6c9a690b80
edriso <edriso.dev@gmail.com>
2023-02-26 23:41:32 +0200

    add productRouter

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
-require('./db/connection');
+// require('./db/connection');
 const express = require('express');
 const cors = require('cors');
 // security packages
 const helmet = require('helmet');
 const xss = require('xss-clean');
+
+const authRouter = require('./routes/authRoutes');
+const productRouter = require('./routes/productRoutes');
+// const orderRouter = require('./routes/orderRoutes');
+const notFound = require('./middleware/notFound');
 
 const app = express();
 
@@ -16,8 +21,14 @@ app.use(xss());
 
 // routes
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('E-commerce API');
 });
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/product', productRouter);
+// app.use('/api/v1/order', orderRouter);
+
+app.use(notFound);
 
 const port = process.env.PORT || 3000;
 
