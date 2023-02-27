90058306593664618ec6e32c665a244ded86670d
edriso <edriso.dev@gmail.com>
2023-02-27 13:29:39 +0200

    fix method name

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,15 @@
 const { StatusCodes } = require('http-status-codes');
 
 const register = (req, res) => {
-  res.statuc(StatusCodes.CREATED).json({ msg: 'user registered' });
+  res.status(StatusCodes.CREATED).json({ msg: 'user registered' });
 };
 
 const login = (req, res) => {
-  res.statuc(StatusCodes.OK).json({ msg: 'user loggedin' });
+  res.status(StatusCodes.OK).json({ msg: 'user loggedin' });
 };
 
 const logout = (req, res) => {
-  res.statuc(StatusCodes.OK).json({ msg: 'user loggedin' });
+  res.status(StatusCodes.OK).json({ msg: 'user loggedin' });
 };
 
 module.exports = {
