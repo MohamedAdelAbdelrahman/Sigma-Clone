9fb3b2f049fa7c080f65ce1befab6e480a371faf
edriso <edriso.dev@gmail.com>
2023-03-01 21:47:33 +0200

    complete register user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,26 @@
-const register = (req, res) => {
-  res.status(201).json({ msg: 'user registered' });
+const User = require('../models/userModel');
+// const jwt = require('jsonwebtoken');
+// const bcryptjs = require('bcryptjs');
+
+const register = async (req, res) => {
+  const { email } = req.body;
+
+  const emailExists = await User.findOne({ email });
+
+  if (emailExists) {
+    return res.status(400).json({ msg: 'Email already exists' });
+  }
+
+  const user = await User.create(req.body);
+
+  res.status(201).json({ user });
 };
 
-const login = (req, res) => {
+const login = async (req, res) => {
   res.status(200).json({ msg: 'user loggedin' });
 };
 
-const logout = (req, res) => {
+const logout = async (req, res) => {
   res.status(200).json({ msg: 'user loggedin' });
 };
 
