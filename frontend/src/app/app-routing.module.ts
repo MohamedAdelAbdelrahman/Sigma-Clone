b30a76717e204f231cd49d50e1cb3fa8f9a03317
Your Name <esraahussein313@example.com>
2023-03-02 15:53:15 +0200

    Adding files

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeadersOfPagesComponent } from './headers-of-pages/headers-of-pages.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:"home",component:HeadersOfPagesComponent},
   {path:"",component:HeadersOfPagesComponent},
+  {path:"login",component:LoginComponent},
 ];
 
 @NgModule({
