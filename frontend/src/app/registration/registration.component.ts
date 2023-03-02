d9cb602d65c4174a447c978172a09b3a0982fd00
Your Name <esraahussein313@example.com>
2023-03-02 20:02:39 +0200

    Adding files

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -8,5 +8,34 @@ import {FormGroup,FormControl,Validators } from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  email="";
+ password="";
 
+    LoginForm= new FormGroup({
+    password: new FormControl("",[Validators.required,Validators.pattern(/^[A-Za-z]\w{7,14}$/)]),
+    username: new FormControl("",[Validators.required,Validators.minLength(7),Validators.maxLength(14)]),
+    email : new FormControl("",[Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),Validators.required])
+  });
+
+  get emailValid(){
+    return this.LoginForm.controls['email'].valid;
+  }
+  get emailTouch(){
+    return this.LoginForm.controls['email'].touched;
+  }
+  get usernameValid(){
+    return this.LoginForm.controls['username'].valid;
+  }
+  get usernameTouch(){
+    return this.LoginForm.controls['username'].touched;
+  }
+  get passwordValid(){
+    return this.LoginForm.controls['password'].valid;
+  }  
+  get passwordTouch(){
+    return this.LoginForm.controls['password'].touched;
+  }
+check(){
+  
+}
 }
