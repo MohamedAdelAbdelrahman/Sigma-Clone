f800fee73db6cb284f34cd6e7aa9e942d4a5b104
edriso <edriso.dev@gmail.com>
2023-03-02 15:31:06 +0200

    add jwt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,17 +1,26 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
   const { email } = req.body;
 
   const emailExists = await User.findOne({ email });
-
   if (emailExists) {
     return res.status(400).json({ msg: 'Email already exists' });
   }
 
   const user = await User.create(req.body);
 
-  res.status(201).json({ user });
+  const tokenUser = {
+    userId: user._id,
+    name: user.name,
+  };
+
+  const token = jwt.sign(tokenUser, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME || '1d',
+  });
+
+  res.status(201).json({ user: tokenUser, token });
 };
 
 const login = async (req, res) => {
