7c1f13637805ee3cebdf776bbe17fafbf957937b
“abdalrhman772 <abdalrhmanmahmoud27@gmail.com>
2023-03-02 16:51:46 +0200

    ui updates

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeadersOfPagesComponent } from './headers-of-pages/headers-of-pages.component';
 import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:"home",component:HeadersOfPagesComponent},
-  {path:"",component:HeadersOfPagesComponent},
+  {path:"home",component:HomeComponent},
+  {path:"",component:HomeComponent},
+  {path:"cart",component:CartComponent},
 ];
 
 @NgModule({
