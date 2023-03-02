7c4e5df025e83c5bcce61316feddce69cb4bf13c
Your Name <esraahussein313@example.com>
2023-03-02 20:03:18 +0200

    Adding files

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -13,7 +13,6 @@ export class LoginComponent {
 
     LoginForm= new FormGroup({
     password: new FormControl("",[Validators.required,Validators.pattern(/^[A-Za-z]\w{7,14}$/)]),
-    username: new FormControl("",[Validators.required,Validators.minLength(7),Validators.maxLength(14)]),
     email : new FormControl("",[Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),Validators.required])
   });
 
