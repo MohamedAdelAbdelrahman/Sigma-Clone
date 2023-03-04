025a1c08215e1b6f31f5075730f5809af77d23f4
edriso <edriso.dev@gmail.com>
2023-03-04 12:38:53 +0200

    change image upload src

--- backend/controllers/UploadsController.js
+++ backend/controllers/UploadsController.js
@@ -1,14 +1,18 @@
-const path = require('path')
+const path = require('path');
 
 const uploadProductImage = async (req, res) => {
-    const productImage = req.files.image;
+  const productImage = req.files.image;
 
-  const imagePath = path.join(__dirname,'../public/uploads/'+`productImage.name`);
-     await productImage.mv(imagePath);
-  return res.status(200).json({image:{src:`/uploads/${productImage.name}`}});
-  
+  const imagePath = path.join(
+    __dirname,
+    '../../frontend/assets/products_images/' + `productImage.name`
+  );
+  await productImage.mv(imagePath);
+  return res
+    .status(200)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = {
   uploadProductImage,
-}
+};
