9adfbf27ed8808c8d79000bb48e582c9cd6463e5
“abdalrhman772 <abdalrhmanmahmoud27@gmail.com>
2023-03-05 04:32:16 +0200

    cart ts

--- frontend/src/app/components/cart/cart.component.ts
+++ frontend/src/app/components/cart/cart.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-
+  constructor(public cartService: CartService) {}
+  cartItems: any;
+  ngOnInit(): void {
+    this.cartService.GetAllCartItems().subscribe({
+      next: (data) => {
+        console.log('cart' + data);
+        this.cartItems = data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
